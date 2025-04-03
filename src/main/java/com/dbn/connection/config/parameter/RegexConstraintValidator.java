141adffb14734d4da9efea7672ceb053605782b7
cameron_bateman <cameron.bateman@oracle.com>
2025-04-03 15:11:51 -0700

    BUGDB-37777710 DBN Connectivity (add regular expression parameter validator)

---
+++ src/main/java/com/dbn/connection/config/parameter/RegexConstraintValidator.java
@@ -0,0 +1,51 @@
+package com.dbn.connection.config.parameter;
+
+import com.dbn.common.properties.ui.PropertiesValidator;
+import com.intellij.openapi.ui.ValidationInfo;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.regex.Pattern;
+
+public class RegexConstraintValidator extends PropertiesValidator {
+
+
+    private final ValidationPattern validationPattern;
+
+    public static class ValidationPattern {
+        private final Pattern pattern;
+        private final String matchMessage;
+
+        public ValidationPattern(Pattern pattern, String matchMessage) {
+            this.pattern = pattern;
+            this.matchMessage = matchMessage;
+        }
+        public ValidationPattern(@NotNull String pattern, String matchMessage) {
+            this(Pattern.compile(pattern), matchMessage);
+        }
+        public String match(Object target) {
+            if (!(target instanceof String)) {
+                return matchMessage;
+            }
+            if (!this.pattern.matcher((String) target).matches()) {
+                return matchMessage;
+            }
+            return null;
+        }
+
+    }
+
+    public RegexConstraintValidator(ValidationPattern validationPattern) {
+        this.validationPattern = validationPattern;
+    }
+    @Override
+    public ValidationInfo validate(String keyName, Object value) {
+        if (! (value instanceof String)) {
+            return new ValidationInfo(keyName + " must be a string");
+        }
+        String message = this.validationPattern.match(value);
+        if (message != null) {
+            return new ValidationInfo(keyName + " must match " + message + " but was " + value);
+        }
+        return null;
+    }
+}
