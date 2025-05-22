90626f2c7dde3579a29a95ad5021b39edbb57e13
Dan Cioca <dan.cioca@oracle.com>
2025-05-22 17:04:10 +0200

    JDBC-3959 - DBN Release Compatibility (gradle build - intellij base version)

--- build.gradle.kts
+++ build.gradle.kts
@@ -110,7 +110,7 @@ sourceSets{
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-  version.set("2024.3.3")
+  version.set("2020.1.1")
   type.set("IC") // Target IDE Platform
 
   plugins.set(listOf("java", "json", "copyright"))
