aff5d953a07c29dab4cf0b068a6804b7f5560bda
Dan Cioca <dan.cioca@oracle.com>
2025-09-11 12:27:17 +0200

    JDBC-4610 OJVM execution (reset java wrapper model on every new execution)

--- src/main/java/com/dbn/execution/java/JavaExecutionManager.java
+++ src/main/java/com/dbn/execution/java/JavaExecutionManager.java
@@ -120,6 +120,7 @@ public class JavaExecutionManager extends ProjectComponentBase implements Persis
 
 	private void promptExecutionDialog(@NotNull DBJavaMethod method, @NotNull DBDebuggerType debuggerType, Runnable callback) {
 		JavaExecutionInput executionInput = getExecutionInput(method);
+        executionInput.resetExecutionContext();
 		promptExecutionDialog(executionInput, debuggerType, callback);
 	}
 
