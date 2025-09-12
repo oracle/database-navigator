769a5adf6af782b3031909a788dedb5fa6a12c51
Dan Cioca <dan.cioca@oracle.com>
2025-09-13 00:28:46 +0200

    JDBC-4643 DB Assistant (view metadata tool)

---
+++ src/main/java/com/dbn/assistant/tool/impl/ViewMetadataToolImpl.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2025 Oracle and/or its affiliates
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dbn.assistant.tool.impl;
+
+import com.dbn.assistant.tool.AssistantToolBase;
+import com.dbn.assistant.tool.spec.ViewMetadataTool;
+import com.dbn.common.util.Commons;
+import com.dbn.editor.DBContentType;
+import com.dbn.editor.code.SourceCodeManager;
+import com.dbn.editor.code.content.SourceCodeContent;
+import com.dbn.object.DBColumn;
+import com.dbn.object.DBMaterializedView;
+import com.dbn.object.DBSchema;
+import com.dbn.object.DBView;
+import com.dbn.object.type.DBObjectType;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+import static com.dbn.common.util.Lists.convert;
+
+public class ViewMetadataToolImpl extends AssistantToolBase implements ViewMetadataTool {
+
+    @Override
+    public List<String> listViewNames(String schemaName) {
+        DBSchema schema = getSchema(schemaName);
+
+        List<DBView> views = schema.getViews();
+        return getObjectNames(views, false);
+    }
+
+    @Override
+    public List<String> listMaterializedViewNames(String schemaName) {
+        DBSchema schema = getSchema(schemaName);
+
+        List<DBMaterializedView> views = schema.getMaterializedViews();
+        return getObjectNames(views, false);
+    }
+
+    @Override
+    public ViewDefinition loadViewDefinition(String schemaName, String viewName, boolean detailed) {
+        DBSchema schema = getSchema(schemaName);
+        return loadViewDefinition(schema, viewName, detailed);
+    }
+
+    @Override
+    public List<ViewDefinition> loadViewDefinitions(String schemaName, List<String> viewNames, boolean detailed) {
+        DBSchema schema = getSchema(schemaName);
+        return convert(viewNames, n -> loadViewDefinition(schema, n, detailed));
+    }
+
+    private ViewDefinition loadViewDefinition(DBSchema schema, String viewName, boolean detailed) {
+        DBView view = Commons.coalesce(
+                () -> schema.getView(viewName),
+                () -> schema.getMaterializedView(viewName));
+
+        view = resolved(view, DBObjectType.VIEW, viewName);
+
+        ViewDefinition viewDef = createDefinition(view);
+        viewDef.setColumns(convert(undisposed(view).getColumns(), c -> createDefinition(c)));
+
+        if (detailed) {
+            try {
+                SourceCodeManager sourceCodeManager = SourceCodeManager.getInstance(view.getProject());
+                SourceCodeContent sourceCode = sourceCodeManager.loadSourceFromDatabase(view, DBContentType.CODE);
+                viewDef.setQuery(sourceCode.getRawContent());
+            } catch (Exception e) {
+                viewDef.setQuery("Error loading view definition: " + e.getMessage());
+            }
+        }
+
+        return viewDef;
+    }
+
+    private static @NotNull ViewDefinition createDefinition(DBView view) {
+        ViewDefinition viewDef = new ViewDefinition();
+        viewDef.setName(view.getQualifiedName());
+        viewDef.setDescription(view.getComments());
+        return viewDef;
+    }
+
+    private static ColumnDefinition createDefinition(DBColumn column) {
+        ColumnDefinition columnDef = new ColumnDefinition();
+        columnDef.setName(column.getName());
+        columnDef.setType(column.getDataType().getName());
+        columnDef.setDescription(column.getComments());
+        return columnDef;
+    }
+}
---
+++ src/main/java/com/dbn/assistant/tool/spec/ViewMetadataTool.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2025 Oracle and/or its affiliates
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dbn.assistant.tool.spec;
+
+import com.dbn.assistant.tool.AssistantTool;
+import com.dbn.assistant.tool.AssistantToolFactoryBase;
+import com.dbn.assistant.tool.AssistantToolInfo.ToolDefinition;
+import com.dbn.assistant.tool.AssistantToolInfo.UtilityDefinition;
+import com.dbn.assistant.tool.impl.ViewMetadataToolImpl;
+import dev.langchain4j.agent.tool.P;
+import dev.langchain4j.agent.tool.Tool;
+import dev.langchain4j.model.output.structured.Description;
+import lombok.Data;
+
+import java.util.List;
+
+import static com.dbn.assistant.tool.AssistantToolCategory.METADATA_PROVIDER;
+import static com.dbn.assistant.tool.AssistantToolInfo.FactoryDefinition;
+
+@ToolDefinition(
+    type = "VIEW_METADATA",
+    name = "View metadata",
+    category = METADATA_PROVIDER,
+    description = "Provides information about the views in a given database schema")
+public interface ViewMetadataTool extends AssistantTool {
+
+    @FactoryDefinition(
+        spec = ViewMetadataTool.class,
+        impl = ViewMetadataToolImpl.class)
+    class Factory extends AssistantToolFactoryBase<ViewMetadataTool> {}
+
+    /*********************************************
+     *                 TOOLS                     *
+     *********************************************/
+
+    @Tool(
+        name = "LIST_VIEW_NAMES",
+        value = {
+            "type=VIEW_METADATA",
+            "category=METADATA_PROVIDER",
+            "Lists view names in a given schema"})
+    @UtilityDefinition(name = "List view names")
+    List<String> listViewNames(
+            @P("Schema name") String schemaName);
+
+
+    @Tool(
+        name = "LIST_MATERIALIZED_VIEW_NAMES",
+        value = {
+                "type=VIEW_METADATA",
+                "category=METADATA_PROVIDER",
+                "Lists materialized view names in a given schema"})
+    @UtilityDefinition(name = "List materialized view names")
+    List<String> listMaterializedViewNames(
+            @P("Schema name") String schemaName);
+
+
+    @Tool(
+        name = "LOAD_VIEW_DEFINITION",
+        value = {
+                "type=VIEW_METADATA",
+                "category=METADATA_PROVIDER",
+                "Loads the definition of a given view"})
+    @UtilityDefinition(name = "Load view definition")
+    ViewDefinition loadViewDefinition(
+            @P("Schema name") String schemaName,
+            @P("View name") String viewName,
+            @P("Include view query (may be slow to respond)") boolean detailed);
+
+    @Tool(
+        name = "LOAD_VIEW_DEFINITIONS",
+        value = {
+                "type=VIEW_METADATA",
+                "category=METADATA_PROVIDER",
+                "Loads the definitions of a given set of views (optimized version of LOAD_VIEW_DEFINITION for bulk queries)"})
+    @UtilityDefinition(name = "Load view definitions")
+    List<ViewDefinition> loadViewDefinitions(
+            @P("Schema name") String schemaName,
+            @P("View names") List<String> viewNames,
+            @P("Include view query (may be slow to respond)") boolean detailed);
+
+
+    @Data
+    @Description("View definition")
+    class ViewDefinition {
+        @Description("View name")
+        private String name;
+
+        @Description("View query")
+        private String query;
+
+        @Description("View description")
+        private String description;
+
+        @Description("Column definitions")
+        private List<ColumnDefinition> columns;
+    }
+
+    @Data
+    @Description("Column definition")
+    class ColumnDefinition {
+        @Description("Column name")
+        private String name;
+
+        @Description("Column data type")
+        private String type;
+
+        @Description("Column description")
+        private String description;
+    }
+}
