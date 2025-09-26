cb44a841272820dfb56128bafe7e3cfbac94264c
Dan Cioca <dan.cioca@oracle.com>
2025-09-26 09:54:21 +0200

    JDBC-4643 DB Assistant (side observations - new island themes / long text-attribute ids)

--- src/main/java/com/dbn/data/grid/color/DataGridTextAttributesKeys.java
+++ src/main/java/com/dbn/data/grid/color/DataGridTextAttributesKeys.java
@@ -25,18 +25,18 @@ import com.intellij.ui.JBColor;
 import java.awt.Color;
 
 public interface DataGridTextAttributesKeys {
-    TextAttributesKey DEFAULT_PLAIN_DATA     = TextAttributesKey.createTextAttributesKey("DBNavigator.DefaultTextAttributes.DataEditor.PlainData");
-    TextAttributesKey DEFAULT_CARET_ROW      = TextAttributesKey.createTextAttributesKey("DBNavigator.DefaultTextAttributes.DataEditor.CaretRow");
-    TextAttributesKey DEFAULT_AUDIT_DATA     = TextAttributesKey.createTextAttributesKey("DBNavigator.DefaultTextAttributes.DataEditor.AuditData");
-    TextAttributesKey DEFAULT_MODIFIED_DATA  = TextAttributesKey.createTextAttributesKey("DBNavigator.DefaultTextAttributes.DataEditor.ModifiedData");
-    TextAttributesKey DEFAULT_DELETED_DATA   = TextAttributesKey.createTextAttributesKey("DBNavigator.DefaultTextAttributes.DataEditor.DeletedData");
-    TextAttributesKey DEFAULT_ERROR_DATA     = TextAttributesKey.createTextAttributesKey("DBNavigator.DefaultTextAttributes.DataEditor.ErrorData");
-    TextAttributesKey DEFAULT_READONLY_DATA  = TextAttributesKey.createTextAttributesKey("DBNavigator.DefaultTextAttributes.DataEditor.ReadonlyData");
-    TextAttributesKey DEFAULT_LOADING_DATA   = TextAttributesKey.createTextAttributesKey("DBNavigator.DefaultTextAttributes.DataEditor.LoadingData");
-    TextAttributesKey DEFAULT_UPDATING_DATA  = TextAttributesKey.createTextAttributesKey("DBNavigator.DefaultTextAttributes.DataEditor.UpdatingData");
-    TextAttributesKey DEFAULT_PRIMARY_KEY    = TextAttributesKey.createTextAttributesKey("DBNavigator.DefaultTextAttributes.DataEditor.PrimaryKey");
-    TextAttributesKey DEFAULT_FOREIGN_KEY    = TextAttributesKey.createTextAttributesKey("DBNavigator.DefaultTextAttributes.DataEditor.ForeignKey");
-    TextAttributesKey DEFAULT_SELECTION      = TextAttributesKey.createTextAttributesKey("DBNavigator.DefaultTextAttributes.DataEditor.Selection");
+    TextAttributesKey DEFAULT_PLAIN_DATA     = TextAttributesKey.createTextAttributesKey("DBN.DefaultAttributes.DataEditor.PlainData");
+    TextAttributesKey DEFAULT_CARET_ROW      = TextAttributesKey.createTextAttributesKey("DBN.DefaultAttributes.DataEditor.CaretRow");
+    TextAttributesKey DEFAULT_AUDIT_DATA     = TextAttributesKey.createTextAttributesKey("DBN.DefaultAttributes.DataEditor.AuditData");
+    TextAttributesKey DEFAULT_MODIFIED_DATA  = TextAttributesKey.createTextAttributesKey("DBN.DefaultAttributes.DataEditor.ModifiedData");
+    TextAttributesKey DEFAULT_DELETED_DATA   = TextAttributesKey.createTextAttributesKey("DBN.DefaultAttributes.DataEditor.DeletedData");
+    TextAttributesKey DEFAULT_ERROR_DATA     = TextAttributesKey.createTextAttributesKey("DBN.DefaultAttributes.DataEditor.ErrorData");
+    TextAttributesKey DEFAULT_READONLY_DATA  = TextAttributesKey.createTextAttributesKey("DBN.DefaultAttributes.DataEditor.ReadonlyData");
+    TextAttributesKey DEFAULT_LOADING_DATA   = TextAttributesKey.createTextAttributesKey("DBN.DefaultAttributes.DataEditor.LoadingData");
+    TextAttributesKey DEFAULT_UPDATING_DATA  = TextAttributesKey.createTextAttributesKey("DBN.DefaultAttributes.DataEditor.UpdatingData");
+    TextAttributesKey DEFAULT_PRIMARY_KEY    = TextAttributesKey.createTextAttributesKey("DBN.DefaultAttributes.DataEditor.PrimaryKey");
+    TextAttributesKey DEFAULT_FOREIGN_KEY    = TextAttributesKey.createTextAttributesKey("DBN.DefaultAttributes.DataEditor.ForeignKey");
+    TextAttributesKey DEFAULT_SELECTION      = TextAttributesKey.createTextAttributesKey("DBN.DefaultAttributes.DataEditor.Selection");
 
     interface Colors {
         Color DEFAULT_BACKGROUND   = HighlighterColors.TEXT.getDefaultAttributes().getBackgroundColor();
@@ -50,16 +50,16 @@ public interface DataGridTextAttributesKeys {
         Color FK_BACKGROUND        = new JBColor(new Color(0xF7FFF7), new Color(0x2A3B2A));
         Color CARET_ROW_BACKGROUND = EditorColorsManager.getInstance().getGlobalScheme().getColor(EditorColors.CARET_ROW_COLOR);
     }
-    TextAttributesKey PLAIN_DATA     = TextAttributesKey.createTextAttributesKey("DBNavigator.TextAttributes.DataEditor.PlainData",    DEFAULT_PLAIN_DATA);
-    TextAttributesKey CARET_ROW      = TextAttributesKey.createTextAttributesKey("DBNavigator.TextAttributes.DataEditor.CaretRow",     DEFAULT_CARET_ROW);
-    TextAttributesKey AUDIT_DATA     = TextAttributesKey.createTextAttributesKey("DBNavigator.TextAttributes.DataEditor.AuditData",    DEFAULT_AUDIT_DATA);
-    TextAttributesKey MODIFIED_DATA  = TextAttributesKey.createTextAttributesKey("DBNavigator.TextAttributes.DataEditor.ModifiedData", DEFAULT_MODIFIED_DATA);
-    TextAttributesKey DELETED_DATA   = TextAttributesKey.createTextAttributesKey("DBNavigator.TextAttributes.DataEditor.DeletedData",  DEFAULT_DELETED_DATA);
-    TextAttributesKey ERROR_DATA     = TextAttributesKey.createTextAttributesKey("DBNavigator.TextAttributes.DataEditor.ErrorData",    DEFAULT_ERROR_DATA);
-    TextAttributesKey READONLY_DATA  = TextAttributesKey.createTextAttributesKey("DBNavigator.TextAttributes.DataEditor.ReadonlyData", DEFAULT_READONLY_DATA);
-    TextAttributesKey LOADING_DATA   = TextAttributesKey.createTextAttributesKey("DBNavigator.TextAttributes.DataEditor.LoadingData",  DEFAULT_LOADING_DATA);
-    TextAttributesKey UPDATING_DATA  = TextAttributesKey.createTextAttributesKey("DBNavigator.TextAttributes.DataEditor.UpdatingData", DEFAULT_UPDATING_DATA);
-    TextAttributesKey PRIMARY_KEY    = TextAttributesKey.createTextAttributesKey("DBNavigator.TextAttributes.DataEditor.PrimaryKey",   DEFAULT_PRIMARY_KEY);
-    TextAttributesKey FOREIGN_KEY    = TextAttributesKey.createTextAttributesKey("DBNavigator.TextAttributes.DataEditor.ForeignKey",   DEFAULT_FOREIGN_KEY);
-    TextAttributesKey SELECTION      = TextAttributesKey.createTextAttributesKey("DBNavigator.TextAttributes.DataEditor.Selection",    DEFAULT_SELECTION);
+    TextAttributesKey PLAIN_DATA     = TextAttributesKey.createTextAttributesKey("DBN.Attributes.DataEditor.PlainData",    DEFAULT_PLAIN_DATA);
+    TextAttributesKey CARET_ROW      = TextAttributesKey.createTextAttributesKey("DBN.Attributes.DataEditor.CaretRow",     DEFAULT_CARET_ROW);
+    TextAttributesKey AUDIT_DATA     = TextAttributesKey.createTextAttributesKey("DBN.Attributes.DataEditor.AuditData",    DEFAULT_AUDIT_DATA);
+    TextAttributesKey MODIFIED_DATA  = TextAttributesKey.createTextAttributesKey("DBN.Attributes.DataEditor.ModifiedData", DEFAULT_MODIFIED_DATA);
+    TextAttributesKey DELETED_DATA   = TextAttributesKey.createTextAttributesKey("DBN.Attributes.DataEditor.DeletedData",  DEFAULT_DELETED_DATA);
+    TextAttributesKey ERROR_DATA     = TextAttributesKey.createTextAttributesKey("DBN.Attributes.DataEditor.ErrorData",    DEFAULT_ERROR_DATA);
+    TextAttributesKey READONLY_DATA  = TextAttributesKey.createTextAttributesKey("DBN.Attributes.DataEditor.ReadonlyData", DEFAULT_READONLY_DATA);
+    TextAttributesKey LOADING_DATA   = TextAttributesKey.createTextAttributesKey("DBN.Attributes.DataEditor.LoadingData",  DEFAULT_LOADING_DATA);
+    TextAttributesKey UPDATING_DATA  = TextAttributesKey.createTextAttributesKey("DBN.Attributes.DataEditor.UpdatingData", DEFAULT_UPDATING_DATA);
+    TextAttributesKey PRIMARY_KEY    = TextAttributesKey.createTextAttributesKey("DBN.Attributes.DataEditor.PrimaryKey",   DEFAULT_PRIMARY_KEY);
+    TextAttributesKey FOREIGN_KEY    = TextAttributesKey.createTextAttributesKey("DBN.Attributes.DataEditor.ForeignKey",   DEFAULT_FOREIGN_KEY);
+    TextAttributesKey SELECTION      = TextAttributesKey.createTextAttributesKey("DBN.Attributes.DataEditor.Selection",    DEFAULT_SELECTION);
 }
