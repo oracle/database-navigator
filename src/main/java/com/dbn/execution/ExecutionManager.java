22e05c4360fc3facb3cd69cec812ca47f8972339
Dan Cioca <dan.cioca@oracle.com>
2025-11-26 16:11:44 +0100

    JDBC-4321 Release 3.7.0 (compatibility branch - merge issues)

--- src/main/java/com/dbn/execution/ExecutionManager.java
+++ src/main/java/com/dbn/execution/ExecutionManager.java
@@ -53,7 +53,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
-import java.util.function.Consumer;
 import java.util.Set;
 
 import static com.dbn.common.component.Components.projectService;
