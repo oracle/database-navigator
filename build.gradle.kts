e8f6b6876d4f1d6480c7dc68c772f7807991fa7c
Dan Cioca <dan.cioca@oracle.com>
2025-11-28 10:22:57 +0100

    JDBC-4838 DBN Dialogs (compatibility - changed earliest supported intellij version)

--- build.gradle.kts
+++ build.gradle.kts
@@ -185,7 +185,7 @@ sourceSets {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2020.1.1")
+    version.set("2022.2.5")
     type.set("IC") // Target IDE Platform
     updateSinceUntilBuild.set(false)
 
