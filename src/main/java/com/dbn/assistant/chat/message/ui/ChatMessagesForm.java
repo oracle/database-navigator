700fc7593096fd397e9cef23d96a96c44c732a8f
Dan Cioca <dan.cioca@oracle.com>
2026-01-06 13:34:08 +0100

    JDBC-5128 - Misc (deprecated Alarm constructor usage)

--- src/main/java/com/dbn/assistant/chat/message/ui/ChatMessagesForm.java
+++ src/main/java/com/dbn/assistant/chat/message/ui/ChatMessagesForm.java
@@ -32,6 +32,7 @@ import com.dbn.common.ui.util.ClientProperty;
 import com.dbn.common.ui.util.Components;
 import com.dbn.common.ui.util.ScrollPanes;
 import com.dbn.common.ui.util.UserInterface;
+import com.dbn.common.util.Alarms;
 import com.dbn.common.util.Lists;
 import com.intellij.util.Alarm;
 import lombok.Getter;
@@ -60,7 +61,7 @@ public class ChatMessagesForm extends DBNFormBase {
     public ChatMessagesForm(@Nullable DBNComponent parent) {
         super(parent);
 
-        scrollAlarm = new Alarm(Alarm.ThreadToUse.SWING_THREAD,this);
+        scrollAlarm = Alarms.createAlarm(this);
         ClientProperty.HORIZONTAL_SCROLL_POLICY.set(messagesScrollPanel, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
         verticalBoxLayout(messagesPanel);
