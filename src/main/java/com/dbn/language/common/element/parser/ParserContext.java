06ab403bb15310bc4f2b5fc60c98ad7964c7c20d
Dan Cioca <dan.cioca@oracle.com>
2026-03-06 10:28:55 +0100

    JDBC-5173 Language Parser (cleanup - "waved-token" obsolete concept)

--- src/main/java/com/dbn/language/common/element/parser/ParserContext.java
+++ src/main/java/com/dbn/language/common/element/parser/ParserContext.java
@@ -18,7 +18,6 @@ package com.dbn.language.common.element.parser;
 
 import com.dbn.language.common.DBLanguageDialect;
 import com.dbn.language.common.SharedTokenTypeBundle;
-import com.dbn.language.common.TokenType;
 import com.dbn.language.common.element.cache.ElementLookupContext;
 import com.dbn.language.common.element.impl.LeafElementType;
 import com.intellij.lang.PsiBuilder;
@@ -27,27 +26,12 @@ public class ParserContext extends ElementLookupContext {
     public final long timestamp = System.currentTimeMillis();
     public final ParserBuilder builder;
     public final SharedTokenTypeBundle sharedTokenTypes;
-    private TokenType wavedTokenType;
-    private int wavedTokenTypeOffset;
 
 
     public ParserContext(PsiBuilder builder, DBLanguageDialect languageDialect, double databaseVersion) {
         super(null, databaseVersion);
         this.builder = new ParserBuilder(builder, languageDialect);
         this.sharedTokenTypes = languageDialect.getSharedTokenTypes();
-    }
-
-    public boolean isWavedTokenType(TokenType tokenType) {
-        return tokenType == wavedTokenType && builder.getOffset() == wavedTokenTypeOffset;
-    }
-
-    public void setWavedTokenType(TokenType wavedTokenType) {
-        this.wavedTokenType = wavedTokenType;
-        this.wavedTokenTypeOffset = builder.getOffset();
-    }
-
-    public boolean isSurrogate() {
-        return builder.tokenMonitor.isSurrogate();
     }
 
     public boolean isSurrogateFor(LeafElementType leafElementType) {
