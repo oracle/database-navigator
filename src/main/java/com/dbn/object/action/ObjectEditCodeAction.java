3d6e20552667a6bcef0684421fe999702b289246
Dan Cioca <dan.cioca@oracle.com>
2026-03-30 17:33:52 +0200

    JDBC-5424 BUGDB-37916818 (database browser bulk actions - initial refactoring)

--- src/main/java/com/dbn/object/action/ObjectEditCodeAction.java
+++ src/main/java/com/dbn/object/action/ObjectEditCodeAction.java
@@ -16,12 +16,10 @@
 
 package com.dbn.object.action;
 
-import com.dbn.common.action.ProjectAction;
 import com.dbn.common.icon.Icons;
 import com.dbn.editor.DatabaseFileEditorManager;
 import com.dbn.editor.EditorProviderId;
 import com.dbn.object.common.DBSchemaObject;
-import com.dbn.object.lookup.DBObjectRef;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.project.Project;
@@ -30,35 +28,20 @@ import org.jetbrains.annotations.Nullable;
 
 import static com.dbn.nls.NlsResources.txt;
 
-public class ObjectEditCodeAction extends ProjectAction {
-    private final DBObjectRef<DBSchemaObject> object;
-
+public class ObjectEditCodeAction extends AnObjectAction<DBSchemaObject> {
     ObjectEditCodeAction(DBSchemaObject object) {
-        this.object = DBObjectRef.of(object);
+        super(object);
     }
 
     @Override
-    protected void update(@NotNull AnActionEvent e, @NotNull Project project) {
-        Presentation presentation = e.getPresentation();
+    protected void update(@NotNull AnActionEvent e, @NotNull Presentation presentation, @NotNull Project project, @Nullable DBSchemaObject object) {
         presentation.setText(txt("app.objects.action.EditCode"));
         presentation.setIcon(Icons.OBJECT_EDIT_SOURCE);
     }
 
-    @Nullable
     @Override
-    public  Project getProject() {
-        return getObject().getProject();
-    }
-
-    @Override
-    public void actionPerformed(@NotNull AnActionEvent e, @NotNull Project project) {
-        DBSchemaObject schemaObject = getObject();
+    protected void actionPerformed(@NotNull AnActionEvent e, @NotNull Project project, @NotNull DBSchemaObject object) {
         DatabaseFileEditorManager editorManager = DatabaseFileEditorManager.getInstance(project);
-        editorManager.connectAndOpenEditor(schemaObject, EditorProviderId.CODE, false, true);
-    }
-
-    @NotNull
-    private DBSchemaObject getObject() {
-        return DBObjectRef.ensure(object);
+        editorManager.connectAndOpenEditor(object, EditorProviderId.CODE, false, true);
     }
 }
