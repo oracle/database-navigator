09d7110a5da76a265bc18b5d2d11891f79f89d9b
Dan Cioca <dan.cioca@oracle.com>
2026-07-05 15:59:24 +0200

    JDBC-4990 Release 4.0 (compatibility build 4.0.0.3)

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ val bundledJdbcPostgres: Configuration by configurations.creating
 val bundledJdbcSqlite: Configuration by configurations.creating
 
 group = "com.dbn"
-version = "4.0.0.0"
+version = "4.0.0.3"
 
 java {
     toolchain {
