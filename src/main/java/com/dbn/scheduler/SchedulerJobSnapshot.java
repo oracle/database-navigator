edc51d6a57d38713e757ce9a3abfe99291789bfb
ayoub <ayoub.allali@oracle.com>
2026-07-08 13:51:44 +0100

    add scheduler model

---
+++ src/main/java/com/dbn/scheduler/SchedulerJobSnapshot.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2026 Oracle and/or its affiliates
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dbn.scheduler;
+
+import lombok.Getter;
+import org.jetbrains.annotations.Nullable;
+
+@Getter
+public class SchedulerJobSnapshot {
+    private final String state;
+    private final String runStatus;
+    private final String errorNumber;
+    private final String additionalInfo;
+    private final SchedulerJobStatus status;
+
+    public SchedulerJobSnapshot(
+            @Nullable String state,
+            @Nullable String runStatus,
+            @Nullable String errorNumber,
+            @Nullable String additionalInfo) {
+        this.state = state;
+        this.runStatus = runStatus;
+        this.errorNumber = errorNumber;
+        this.additionalInfo = additionalInfo;
+        this.status = SchedulerJobStatus.resolve(state, runStatus);
+    }
+
+    public static SchedulerJobSnapshot notFound() {
+        return new SchedulerJobSnapshot(null, null, null, null);
+    }
+}
---
+++ src/main/java/com/dbn/scheduler/SchedulerJobStatus.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2026 Oracle and/or its affiliates
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dbn.scheduler;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Locale;
+
+public enum SchedulerJobStatus {
+    SCHEDULED,
+    RUNNING,
+    COMPLETED,
+    SUCCEEDED,
+    FAILED,
+    BROKEN,
+    STOPPED,
+    UNKNOWN;
+
+    public boolean isTerminal() {
+        return isOneOf(SUCCEEDED, FAILED, BROKEN, STOPPED);
+    }
+
+    public boolean isSuccessful() {
+        return this == SUCCEEDED;
+    }
+
+    public boolean isOneOf(SchedulerJobStatus... statuses) {
+        for (SchedulerJobStatus status : statuses) {
+            if (this == status) return true;
+        }
+        return false;
+    }
+
+    public static SchedulerJobStatus resolve(@Nullable String state, @Nullable String runStatus) {
+        SchedulerJobStatus status = valueOf(runStatus);
+        return status == UNKNOWN ? valueOf(state) : status;
+    }
+
+    private static SchedulerJobStatus valueOf(@Nullable String value) {
+        try {
+            return value == null ? UNKNOWN : SchedulerJobStatus.valueOf(value.trim().toUpperCase(Locale.ROOT));
+        } catch (IllegalArgumentException ignore) {
+            return UNKNOWN;
+        }
+    }
+}
