cd87c3c59d861fda846f23484f18939e9a986d24
Dan Cioca <dan.cioca@oracle.com>
2026-07-17 04:38:14 +0200

    JDBC-6365 Liquibase integration (DROP ALL action icon)

--- src/main/java/com/dbn/liquibase/execution/LiquibaseOperation.java
+++ src/main/java/com/dbn/liquibase/execution/LiquibaseOperation.java
@@ -52,6 +52,7 @@ public enum LiquibaseOperation implements Constant<LiquibaseOperation> {
             case GENERATE_DIFF_CHANGELOG -> AllIcons.Actions.Diff;
             case UPDATE_DATABASE -> Icons.ACTION_UPLOAD;
             case ROLLBACK_CHANGESETS -> Icons.ACTION_REVERT;
+            case DROP_ALL -> Icons.ACTION_DELETE;
             default -> null;
         };
     }
