e1191d5ecd845e5036bd52b94eccd848222fdfcf
Dan Cioca <dan.cioca@oracle.com>
2026-07-28 19:18:13 +0200

    JDBC-6365 Liquibase integration (environment profiles - default environmen type name)

--- src/main/java/com/dbn/common/environment/EnvironmentType.java
+++ src/main/java/com/dbn/common/environment/EnvironmentType.java
@@ -61,7 +61,7 @@ public class EnvironmentType implements Cloneable<EnvironmentType>, PersistentCo
         JBColor NONE = new JBColor(new Color(0xffffff), Color.DARK_GRAY);
     }
 
-    public static final EnvironmentType DEFAULT     = new EnvironmentType(EnvironmentTypeId.DEFAULT, "", "", new Color(214,214,214), new Color(72,74,74), false, false);
+    public static final EnvironmentType DEFAULT     = new EnvironmentType(EnvironmentTypeId.DEFAULT, txt("cfg.environment.const.EnvironmentType_DEFAULT"), txt("cfg.environment.text.DefaultEnvironment"), new Color(214,214,214), new Color(72,74,74), false, false);
     public static final EnvironmentType DEVELOPMENT = new EnvironmentType(EnvironmentTypeId.DEVELOPMENT, txt("cfg.environment.const.EnvironmentType_DEVELOPMENT"), txt("cfg.environment.text.DevelopmentEnvironment"), new Color(-2430209), new Color(0x445F80), false, false);
     public static final EnvironmentType INTEGRATION = new EnvironmentType(EnvironmentTypeId.INTEGRATION, txt("cfg.environment.const.EnvironmentType_INTEGRATION"), txt("cfg.environment.text.IntegrationEnvironment"), new Color(-2621494), new Color(0x466646), true, false);
     public static final EnvironmentType PRODUCTION  = new EnvironmentType(EnvironmentTypeId.PRODUCTION, txt("cfg.environment.const.EnvironmentType_PRODUCTION"), txt("cfg.environment.text.ProductionEnvironment"), new Color(-11574), new Color(0x634544), true, true);
