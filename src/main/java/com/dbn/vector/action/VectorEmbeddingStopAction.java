37fd41ec54f1d4ec9f3e070df7c943bc2b352ef6
Dan Cioca <dan.cioca@oracle.com>
2026-07-29 23:52:02 +0200

    JDBC-6357 Vector Toolbox (stop action)

---
+++ src/main/java/com/dbn/vector/action/VectorEmbeddingStopAction.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2026 Oracle and/or its affiliates
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dbn.vector.action;
+
+import com.dbn.common.icon.Icons;
+import com.dbn.vector.model.VectorEmbeddingExecutionResult;
+import com.dbn.vector.model.VectorEmbeddingResult;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import static com.dbn.nls.NlsResources.txt;
+
+public class VectorEmbeddingStopAction extends AbstractVectorEmbeddingResultAction {
+    public VectorEmbeddingStopAction() {
+        super(txt("action.DBN.Vector.Embedding.Result.Stop.text"));
+    }
+
+    @Override
+    protected void actionPerformed(
+            @NotNull AnActionEvent e,
+            @NotNull Project project,
+            @NotNull VectorEmbeddingExecutionResult executionResult) {
+        executionResult.getVectorEmbeddingResult().cancel();
+    }
+
+    @Override
+    protected void update(
+            @NotNull AnActionEvent e,
+            @NotNull Presentation presentation,
+            @NotNull Project project,
+            @Nullable VectorEmbeddingExecutionResult target) {
+        presentation.setText(txt("action.DBN.Vector.Embedding.Result.Stop.text"));
+        presentation.setIcon(Icons.ACTION_STOP);
+        presentation.setEnabled(target != null &&
+                target.getVectorEmbeddingResult().getStatus() == VectorEmbeddingResult.Status.RUNNING);
+    }
+}
